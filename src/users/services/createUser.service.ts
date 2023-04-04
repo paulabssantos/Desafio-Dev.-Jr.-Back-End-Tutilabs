ba4f5e299fa82c6e69573ad335711c2058b6aa9c
paulabssantos <paulablsantos7@gmail.com>
2023-04-04 13:33:51 -0400

    feat: cria usuario

--- src/users/services/createUser.service.ts
+++ src/users/services/createUser.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from "@nestjs/common";
-import { CreateUserDto } from "../dto/create-user.dto";
-import { UserRepository } from "../repository/UserRepository";
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UserRepository } from '../../main/config/db/repositories/users/UserRepository';
 
 @Injectable()
-export class CreateUserService{
-    constructor(private readonly userRepository: UserRepository){}
-    async execute({email,fk_roles,name,password}: CreateUserDto){
-        return await this.userRepository.create({email,fk_roles,name,password})
-    }
+export class CreateUserService {
+  constructor(private readonly userRepository: UserRepository) {}
+  async execute({ email, fk_roles, name, password }: CreateUserDto) {
+    return await this.userRepository.create({
+      email,
+      fk_roles,
+      name,
+      password,
+    });
+  }
 }
