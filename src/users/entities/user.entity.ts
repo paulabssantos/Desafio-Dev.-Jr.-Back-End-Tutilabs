57615f5db248b7fd9d09ef5e2ba9ef30536c92ae
paulabssantos <paulablsantos7@gmail.com>
2023-04-04 13:41:34 -0400

    style: organiza codigo

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,7 @@
 export class User {
-    id: string;
-    name: string;
-    fk_roles: string;
-    password: string;
-    email: string;
+  id: string;
+  name: string;
+  fk_roles: string;
+  password: string;
+  email: string;
 }
--- src/users/repository/UserRepository.ts
+++
@@ -1,5 +0,0 @@
-import { CreateUserDto } from "../dto/create-user.dto";
-
-export abstract class UserRepository{
-    abstract create(createUserDTO: CreateUserDto):Promise<void>;
-}
