d43dc7eca45e92ab931cf5b39abac898bfd336e8
Paula Beatriz Lima dos Santos <you@example.com>
2023-04-04 15:37:34 -0400

    feat: valida se email ja esta cadastrado antes de criar usuario

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,20 +1,18 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
-    @IsString({message: "Nome precisa ser uma string"})
-    @IsNotEmpty({message: "Nome é obrigatório"})
+    @IsString({ message: "Nome precisa ser uma string" })
+    @IsNotEmpty({ message: "Nome é obrigatório" })
     name: string;
 
 
-    @IsString({message: "Id do nível de acesso precisa ser uma string"})
-    @IsNotEmpty({message: "Id dpo nível de acesso é obrigatório"})
+    @IsString({ message: "Id do nível de acesso precisa ser uma string" })
+    @IsNotEmpty({ message: "Id dpo nível de acesso é obrigatório" })
     fk_roles: string;
 
-    @IsString({message: "Senha precisa ser uma string"})
-    @IsNotEmpty({message: "Senha é obrigatória"})
-    password: string;
+    password?: string;
 
-    @IsEmail({},{message: "Email precisa ser válido"})
-    @IsNotEmpty({message: "Email é obrigatório"})
+    @IsEmail({}, { message: "Email precisa ser válido" })
+    @IsNotEmpty({ message: "Email é obrigatório" })
     email: string;
 }
--- src/users/services/createUser.service.ts
+++ src/users/services/createUser.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserRepository } from 'src/config/database/repositories/users/UserRepository';
 import { HashService } from 'src/utils/hash/hash.service';
@@ -6,8 +6,17 @@ import { HashService } from 'src/utils/hash/hash.service';
 @Injectable()
 export class CreateUserService {
   constructor(private readonly userRepository: UserRepository, private readonly hashService: HashService) { }
-  async execute({ email, fk_roles, name, password }: CreateUserDto) {
-    const hash_password = await this.hashService.execute(password)
+  async execute({ email, fk_roles, name }: CreateUserDto) {
+    if (email) {
+      const email_exists = await this.userRepository.findByEmail(email);
+      if (email_exists) {
+        throw new HttpException(
+          'Email já vinculado a um usuário',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+    }
+    const hash_password = await this.hashService.execute('12345')
 
     return await this.userRepository.create({
       email,
