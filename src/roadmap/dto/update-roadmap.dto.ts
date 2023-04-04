a584d0428f970c450d8099b5bbcf49f93919f53a
Paula Beatriz Lima dos Santos <you@example.com>
2023-04-04 17:45:18 -0400

    feat: implementa interfaces de roteiro

---
+++ src/roadmap/dto/update-roadmap.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateRoadmapDto } from './create-roadmap.dto';
+
+export class UpdateRoadmapDto extends PartialType(CreateRoadmapDto) {}
---
+++ src/roadmap/entities/roadmap.entity.ts
@@ -0,0 +1,8 @@
+export class Roadmap {
+    id: string;
+    title: string;
+    orcamento_proposto: number;
+    file: string;
+    risco: string;
+    fk_produtora: string
+}
