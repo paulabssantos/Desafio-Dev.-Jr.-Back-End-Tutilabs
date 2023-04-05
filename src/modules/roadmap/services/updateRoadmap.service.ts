4c7699dc347e083f6cb6d3862e8b2e54649b57cf
Paula Beatriz Lima dos Santos <you@example.com>
2023-04-05 17:42:09 -0400

    style: divide remocao de arquivo em uma funcao

--- src/modules/roadmap/services/updateRoadmap.service.ts
+++ src/modules/roadmap/services/updateRoadmap.service.ts
@@ -8,42 +8,33 @@ export class UpdateRoadmapService {
     constructor(private roadmapRepository: RoadmapRepository, private userRepository: UserRepository) { }
 
     async execute(id: string, file: Express.Multer.File, updateRoadmapDto: UpdateRoadmapDto) {
-        const roadmap = await this.roadmapRepository.find(id)
-        if (!roadmap) {
-            fs.unlink(file.path, (err) => {
+        const deleteFile = (filePath: string) => {
+            fs.unlink(filePath, (err) => {
                 if (err) {
                     console.log(err)
                 }
             })
+        }
+
+        const roadmap = await this.roadmapRepository.find(id)
+
+        if (!roadmap) {
+            deleteFile(file.path)
             throw new HttpException('Roteiro não encontrado', HttpStatus.NOT_FOUND)
         }
         if (file) {
-            if (roadmap.file) {
-                fs.unlink(roadmap.file, (err) => {
-                    if (err) {
-                        console.log(err)
-                    }
-                })
-            }
+            deleteFile(roadmap.file)
         }
         if (updateRoadmapDto.fk_produtora) {
             const user = await this.userRepository.find({ id: updateRoadmapDto.fk_produtora });
             if (user) {
                 if (user.fk_roles != '2') {
-                    fs.unlink(file.path, (err) => {
-                        if (err) {
-                            console.log(err)
-                        }
-                    })
+                    deleteFile(file.path)
                     throw new HttpException('Usuário precisa ser uma produtora', HttpStatus.BAD_REQUEST)
                 }
             }
             else {
-                fs.unlink(file.path, (err) => {
-                    if (err) {
-                        console.log(err)
-                    }
-                })
+                deleteFile(file.path)
                 throw new HttpException('Usuário não encontrado', HttpStatus.NOT_FOUND)
             }
         }
