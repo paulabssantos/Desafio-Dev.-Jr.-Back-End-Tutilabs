7107c625bd5d18496b5df795c8342f7a43641375
Paula Beatriz Lima dos Santos <you@example.com>
2023-04-06 17:14:16 -0400

    feat: inclui modulo de homologacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from '../modules/users/users.module';
 import { RoadmapModule } from '../modules/roadmap/roadmap.module';
+import { HomologationModule } from 'src/modules/homologation/homologation.module';
 
 @Module({
-  imports: [UsersModule, RoadmapModule],
+  imports: [UsersModule, RoadmapModule, HomologationModule],
   controllers: [],
   providers: [],
   exports: []
