41beb20a5380dfba7300798043c7bb68da79db7c
paulabssantos <paulablsantos7@gmail.com>
2023-04-09 20:56:33 -0400

    feat: hasheia senha ao atualizar usuario

--- src/modules/users/services/updateUser.service.ts
+++ src/modules/users/services/updateUser.service.ts
@@ -2,10 +2,11 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UpdateUserDto } from '../dto/update-user.dto';
 import { UserRepository } from 'src/app/config/database/repositories/users/UserRepository';
 import { roles } from 'src/modules/authentication/enum/roles.enum';
+import { Hash } from 'src/utils/hash/hash';
 
 @Injectable()
 export class UpdateUserService {
-  constructor(private readonly userRepository: UserRepository) { }
+  constructor(private readonly userRepository: UserRepository, private hashService: Hash) { }
   async execute(
     id: string,
     { email, fk_roles, name, password }: UpdateUserDto,
@@ -25,11 +26,16 @@ export class UpdateUserService {
       throw new HttpException('O nível de acesso informado é inválido. Os níveis são admin, produtora e roteirista', HttpStatus.BAD_REQUEST)
     }
 
+    let hash_password = undefined
+    if (password) {
+      hash_password = await this.hashService.hash(password)
+    }
+
     return await this.userRepository.update(id, {
       email,
       fk_roles,
       name,
-      password,
+      password: hash_password,
     });
   }
 }
